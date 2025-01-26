00f727b55fc21eb1680f17379f57198a966f7d4d
Ankit-cs <ankitcareer018@gmail.com>
2025-01-26 18:58:17 +0530

    kk

---
+++ BASICS/app.js
@@ -0,0 +1,30 @@
+const express=require('express');
+const app=express();
+
+// routes create karna hai isliye / ka use hota hai
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(function(req,res,next){
+    console.log("middleware chala");
+    next();
+});
+
+app.use(function(req,res,next){
+    console.log("middleware chala eke aur baar ");
+    next();
+});
+
+
+app.get('/',function(req,res){
+    res.send("haanji bhadiya sab theek toh hai na ");
+})
+
+app.get('/rom',function(req,res){
+    res.send("ram rom");
+})
+app.use((err,req,res,next)=>{
+    console.log(err.stack)
+    res.status(500).send('Something broke!m')
+})
+app.listen(3000);
