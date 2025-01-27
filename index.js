345849fdf6039dfe875a11472db5b98b0b2bb714
Ankit-cs <ankitcareer018@gmail.com>
2025-01-27 18:57:19 +0530

    just leanred what is dynamic routing and how to create routes dynamic

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-// console.log(__dirname+'/public')
 const express = require('express');
-const path=require('path');
+const path = require('path');
 const app = express();
 
-/* Both lines are used for parsing request cookies  */
+// Parsing request cookies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/public',express.static(path.join(__dirname,'public')));
-app.set('view engine','ejs')
 
-// app.get("/", function(req, res) {
-//     res.send("chal raha hai ");
-// });
+// Serve static files from the "public" folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.get("/", function(req, res) {
-    res.render("index"); // No need to include ./ or .ejs
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Define routes with dynamic parameters
+app.get("/profile/:username", function(req, res) {
+    res.send(`Welcome, ${req.params.username}`);
 });
 
+app.get("/profile/:username/:age/:rno/:college/:livein", function(req, res) {
+    res.send(`Welcome, ${req.params.username} of age ${req.params.age} with rno ${req.params.rno} studyin in university ${req.params.college} live in ${req.params.livein}`);
+});
 
+// Route for rendering the index.ejs file
+app.get("/", function(req, res) {
+    res.render("index"); // Render the index.ejs file
+});
+
+// Start the server
 app.listen(3000, function() {
     console.log('Server is running on port 3000');
 });
